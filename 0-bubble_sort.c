42707240b9f773bf706d2024693405881c1af60b
TerryEmo <terrance.sabelo@gmail.com>
2023-10-21 09:07:43 -0700

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,34 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * swap_ints - Swaping two integers in one array
- * @a: The first integer to be swaped
- * @b: The second integer to be swaped as well.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sorting out all the array of the integers in ascending arragement
- * @array: A array of integers to be sorted
+ * bubble_sort - sorts the array of the integers in ascending orders
+ * @array: The array of integer
  * @size: The size of the array
  *
- * Description: Prints the array after each swap.
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i;
+	int tmp, sorted;
 
-	if (array == NULL || size < 2)
+	if (!array || size == 0)
 		return;
 
-	while (bubbly == false)
-	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+	do {
+		sorted = 1;
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				sorted = 0;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 				print_array(array, size);
-				bubbly = false;
 			}
 		}
-		len--;
-	}
+
+	} while (sorted == 0);
 }
